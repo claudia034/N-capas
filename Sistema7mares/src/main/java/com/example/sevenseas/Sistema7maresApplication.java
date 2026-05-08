c9edb5e38fb948ad801a789d8c8e1d258474e37f
clau034 <00037221@uca.edu.sv>
2026-05-07 23:17:03 -0600

    feat: create Spring Boot pirates project structure

---
+++ Sistema7mares/src/main/java/com/example/sevenseas/Sistema7maresApplication.java
@@ -0,0 +1,13 @@
+package com.example.sevenseas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sistema7maresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sistema7maresApplication.class, args);
+    }
+
+}
